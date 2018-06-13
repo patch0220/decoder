bf552ecfc8744613adfd7650ebd6d6fe3f32315e
Patrich Paolo Datu <pbdatu@rogomi.com>
2018-06-13 15:43:14 +0800

    Fixed formatting

--- solution.go
+++ solution.go
@@ -1,12 +1,12 @@
 /*
-    Author: Patrich Paolo Datu
-    Notes: 
-    This solution only solves for a small set of keys(e.g. the sample inputs), up to 27 keys only.
-    I am still unsure on what to do with the remaining message after reading a zero length segment. I just relied on this part "the message is decoded by translating the keys in the segments one-at-a-time into the header characters to which they have been mapped."
-    
-    TODOs:
-    - Readline for input
-    - create a finite 2d array for storing the map instead of using a hardcoded map, not relying on the string value but on the decimal value of the binary key when converted.
+   Author: Patrich Paolo Datu
+   Notes:
+   This solution only solves for a small set of keys(e.g. the sample inputs), up to 27 keys only.
+   I am still unsure on what to do with the remaining message after reading a zero length segment. I just relied on this part "the message is decoded by translating the keys in the segments one-at-a-time into the header characters to which they have been mapped."
+
+   TODOs:
+   - Readline for input
+   - create a finite 2d array for storing the map instead of using a hardcoded map, not relying on the string value but on the decimal value of the binary key when converted.
 */
 
 package main
@@ -18,33 +18,33 @@ import (
 
 func main() {
 	hashmap := map[string]string{
-		"0":   "",
-		"00":  "",
-		"01":  "",
-		"10":  "",
-		"000": "",
-		"001": "",
-		"010": "",
-		"011": "",
-		"100": "",
-		"101": "",
-		"110": "",
-        "0000": "",
-        "0001": "",
-        "0010": "",
-        "0011": "",
-        "0100": "",
-        "0101": "",
-        "0110": "",
-        "0111": "",
-        "1000": "",
-        "1001": "",
-        "1010": "",
-        "1011": "",
-        "1100": "",
-        "1101": "",
-        "1110": "",
-        "00000": ""}
+		"0":     "",
+		"00":    "",
+		"01":    "",
+		"10":    "",
+		"000":   "",
+		"001":   "",
+		"010":   "",
+		"011":   "",
+		"100":   "",
+		"101":   "",
+		"110":   "",
+		"0000":  "",
+		"0001":  "",
+		"0010":  "",
+		"0011":  "",
+		"0100":  "",
+		"0101":  "",
+		"0110":  "",
+		"0111":  "",
+		"1000":  "",
+		"1001":  "",
+		"1010":  "",
+		"1011":  "",
+		"1100":  "",
+		"1101":  "",
+		"1110":  "",
+		"00000": ""}
 
 	keys := [...]string{
 		"0",
@@ -58,22 +58,22 @@ func main() {
 		"100",
 		"101",
 		"110",
-        "0000",
-        "0001",
-        "0010",
-        "0011",
-        "0100",
-        "0101",
-        "0110",
-        "0111",
-        "1000",
-        "1001",
-        "1010",
-        "1011",
-        "1100",
-        "1101",
-        "1110",
-        "00000"}
+		"0000",
+		"0001",
+		"0010",
+		"0011",
+		"0100",
+		"0101",
+		"0110",
+		"0111",
+		"1000",
+		"1001",
+		"1010",
+		"1011",
+		"1100",
+		"1101",
+		"1110",
+		"00000"}
 	array := [...]string{"TNM AEIOU",
 		"0010101100011",
 		"1010001001110110011",
@@ -109,18 +109,18 @@ func main() {
 		first := 0
 		for {
 			// Get first 3 and evaluate length
-			last := first + 3			
+			last := first + 3
 			length := length(messages[i][first:last])
 			if length != 0 {
 				// Loop until substring is equal to ones(length)
 				first = last
 				last = last + length
 				for {
-					// fmt.Println("Checking map: ", messages[i][first:last], hashmap[messages[i][first:last]])	
-					if (messages[i][first:last] == ones(length)) {
+					// fmt.Println("Checking map: ", messages[i][first:last], hashmap[messages[i][first:last]])
+					if messages[i][first:last] == ones(length) {
 						first = last
 						break
-					} else {		
+					} else {
 						fmt.Printf(hashmap[messages[i][first:last]])
 						first = last
 						last = last + length
@@ -130,13 +130,13 @@ func main() {
 				// Translating keys one at a time
 				first = last
 				last = last + 1
-				if (last < len(messages[i])) {
+				if last < len(messages[i]) {
 					for {
-						// fmt.Println("Checking map: ", messages[i][first:last], hashmap[messages[i][first:last]])	
-						if (messages[i][first:last] == ones(1)) {
+						// fmt.Println("Checking map: ", messages[i][first:last], hashmap[messages[i][first:last]])
+						if messages[i][first:last] == ones(1) {
 							first = last
 							break
-						} else {		
+						} else {
 							fmt.Printf(hashmap[messages[i][first:last]])
 							first = last
 							last = last + 1
@@ -144,7 +144,7 @@ func main() {
 					}
 				} else {
 					fmt.Printf("\n")
-					break;
+					break
 				}
 			}
 		}
